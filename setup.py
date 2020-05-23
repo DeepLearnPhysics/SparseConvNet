5968d1d49a483152173128588978178a41713345
drinkingkazu <kazuhiro@nevis.columbia.edu>
2020-05-22 22:08:38 -0700

    no check on cuda to build on non-gpu docker

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ import torch, os
 from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension
 from setuptools import setup, find_packages
 
-if torch.cuda.is_available():
-    assert torch.matmul(torch.ones(2097153,2).cuda(),torch.ones(2,2).cuda()).min().item()==2, 'Please upgrade from CUDA 9.0 to CUDA 10.0+'
+#if torch.cuda.is_available():
+#    assert torch.matmul(torch.ones(2097153,2).cuda(),torch.ones(2,2).cuda()).min().item()==2, 'Please upgrade from CUDA 9.0 to CUDA 10.0+'
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 torch_dir = os.path.dirname(torch.__file__)
@@ -17,8 +17,8 @@ conda_include_dir = '/'.join(torch_dir.split('/')[:-4]) + '/include'
 extra = {'cxx': ['-std=c++14', '-fopenmp'], 'nvcc': ['-std=c++14', '-Xcompiler', '-fopenmp']}
 
 setup(
-    name='scn-cuda10',
-    version='0.2.12182018',
+    name='scn-cuda10.2',
+    version='0.2.05222020',
     description='Submanifold (Spatially) Sparse Convolutional Networks https://arxiv.org/abs/1706.01307, original author Benjamin Graham (benjamingraham@fb.com',
     author='Facebook AI Research (forked and sheel distributed by DLP)',
     author_email='contact@deeplearnphysics.org',
@@ -30,11 +30,12 @@ setup(
          'sparseconvnet/SCN/cuda.cu', 'sparseconvnet/SCN/sparseconvnet_cuda.cpp', 'sparseconvnet/SCN/pybind.cpp'],
         include_dirs=[this_dir+'/sparseconvnet/SCN/'],
         extra_compile_args=extra)
-      if torch.cuda.is_available()  else
-      CppExtension('sparseconvnet.SCN',
-        ['sparseconvnet/SCN/pybind.cpp', 'sparseconvnet/SCN/sparseconvnet_cpu.cpp'],
-        include_dirs=[this_dir+'/sparseconvnet/SCN/'],
-        extra_compile_args=extra['cxx'])],
+      #if torch.cuda.is_available()  else
+      #CppExtension('sparseconvnet.SCN',
+      #  ['sparseconvnet/SCN/pybind.cpp', 'sparseconvnet/SCN/sparseconvnet_cpu.cpp'],
+      #  include_dirs=[this_dir+'/sparseconvnet/SCN/'],
+      #  extra_compile_args=extra['cxx'])
+    ],
     cmdclass={'build_ext': BuildExtension},
     zip_safe=False,
 )
